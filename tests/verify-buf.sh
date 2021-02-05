2d3bfa9b124712732cde7fbda85d41da9ad2f4de
Randolf C <34705014+jun-sheaf@users.noreply.github.com>
2021-02-05 02:42:55 +0100

    fix: buf needs to fix versioning

--- tests/verify-buf.sh
+++ tests/verify-buf.sh
@@ -5,8 +5,9 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
-buf_version=$(buf --version)
-echo "Found buf version $buf_version"
+buf_version=$(buf --version 2>&1)
+
+echo "Found Buf version '$buf_version'"
 if [ -z "$(echo $buf_version | grep $1)" ]; then
   echo "Unexpected version"
   exit 1
