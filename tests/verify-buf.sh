6499f1bdb64502d126fbcb5f91ad83b04107266a
Randolf C <34705014+jun-sheaf@users.noreply.github.com>
2021-02-05 02:30:21 +0100

    fix: remove bad single-quotes

--- tests/verify-buf.sh
+++ tests/verify-buf.sh
@@ -5,8 +5,8 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
-buf_version="$(buf --version)"
-echo "Found buf version '$buf_version'"
+buf_version=$(buf --version)
+echo "Found buf version $buf_version"
 if [ -z "$(echo $buf_version | grep $1)" ]; then
   echo "Unexpected version"
   exit 1
